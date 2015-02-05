a05235de55c159be277d1fc9254a17de3a9b54e3
Danilo Shiga <daniloshiga@gmail.com>
2015-02-05 17:36:52 -0200

    Nova view para fazer o redirecionamento

--- oscar_pagseguro/urls.py
+++ oscar_pagseguro/urls.py
@@ -1,14 +1,17 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import *
 
-from .views import SuccessResponseView, ReceiveNotification
+from .views import (SuccessResponseView, ReceiveNotification,
+                    PagseguroRedirectView)
 
 urlpatterns = patterns('',
+    url(r'^redirect/', PagseguroRedirectView.as_view(),
+        name='pagseguro-redirect'),
     url(r'^preview/(?P<basket_id>\d+)/$',
         SuccessResponseView.as_view(preview=True),
         name='pagseguro-success-response'),
     url(r'^checkout/payment-details/$',
-        SuccessResponseView.as_view(preview=True),
+        SuccessResponseView.as_view(preview=False),
         name='pagseguro-success-response'),
     url(r'^checkout/preview/$',
         SuccessResponseView.as_view(preview=True),
--- oscar_pagseguro/views.py
+++ oscar_pagseguro/views.py
@@ -1,11 +1,12 @@
 # -*- coding: utf-8 -*-
 from django.http import HttpResponse
-from django.views.generic import View
+from django.views.generic import RedirectView, View
 from django.utils.decorators import method_decorator
 from django.views.decorators.csrf import csrf_exempt
 
 import six
 
+from oscar.apps.payment import models
 from oscar.apps.payment.exceptions import RedirectRequired
 from oscar.core.loading import get_class
 
@@ -13,16 +14,16 @@ from pagseguro.api import PagSeguroItem, PagSeguroApi
 
 PaymentDetailsView = get_class('checkout.views', 'PaymentDetailsView')
 OrderPlacementMixin = get_class('checkout.mixins', 'OrderPlacementMixin')
+CheckoutSessionMixin = get_class('checkout.session', 'CheckoutSessionMixin')
 
 
-class SuccessResponseView(PaymentDetailsView):
+class PagseguroRedirectView(CheckoutSessionMixin, RedirectView):
+    permanent = False
 
-    def handle_payment(self, order_number, total, **kwargs):
-        """Handles a payment related to pagseguro"""
-
-        pagseguro_api = PagSeguroApi(reference=order_number)
+    def get_redirect_url(self, *args, **kwargs):
 
         basket = self.request.basket
+        pagseguro_api = PagSeguroApi(reference=basket.id)
 
         import ipdb
         ipdb.set_trace()
@@ -40,9 +41,24 @@ class SuccessResponseView(PaymentDetailsView):
 
         data = pagseguro_api.checkout()
 
-        self.request.session['checkout_order_id'] = order_number
+        return data['redirect_url']
 
-        raise RedirectRequired(url=data['redirect_url'])
+
+class SuccessResponseView(PaymentDetailsView):
+
+    def handle_payment(self, order_number, total, **kwargs):
+        """Handles a payment related to pagseguro"""
+
+        source_type, __ = models.SourceType.objects.get_or_create(
+            name="Pagseguro")
+        source = models.Source(
+            source_type=source_type,
+            amount_allocated=total.incl_tax,
+            reference=order_number)
+        self.add_payment_source(source)
+
+        # Record payment event
+        self.add_payment_event('pre-auth', total.incl_tax)
 
 
 class ReceiveNotification(OrderPlacementMixin, View):
