f45a4770da053ac9335df2fb9d676986773c1820
Danilo Shiga <daniloshiga@gmail.com>
2015-02-12 09:49:45 -0200

    Removing view that isn't used anymore

--- oscar_pagseguro/urls.py
+++ oscar_pagseguro/urls.py
@@ -1,11 +1,9 @@
 # -*- coding: utf-8 -*-
 from django.conf.urls import *
 
-from .views import (SuccessResponseView, ReceiveNotification,
-                    PagseguroRedirectView)
+from .views import (SuccessResponseView, ReceiveNotification)
 
 urlpatterns = patterns('',
-    url(r'^redirect/', PagseguroRedirectView.as_view(), name='pagseguro-redirect'),
     url(r'^preview/(?P<basket_id>\d+)/$', SuccessResponseView.as_view(preview=True), name='pagseguro-success-response'),
     url(r'^checkout/payment-details/$', SuccessResponseView.as_view(preview=True), name='pagseguro-success-response'),
     url(r'^checkout/preview/$', SuccessResponseView.as_view(preview=True), name='pagseguro-success-response'),
